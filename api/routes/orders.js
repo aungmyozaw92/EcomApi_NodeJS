0a889092e7465efc5d3be4737d377c62de6631ad
aungmyozaw <aungmyo.zaw@code2lab.com>
2019-01-08 14:10:24 +0630

    update node js product and order

---
+++ api/routes/orders.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+//Handle incoming Get request to orders
+router.get('/', (req, res, next)=>{
+    res.status(200).json({
+        message: 'orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next)=>{
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'orders were created',
+        order: order
+    });
+});
+
+router.get('/:orderId', (req, res, next)=>{
+    res.status(200).json({
+        message: 'orders details',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next)=>{
+    res.status(200).json({
+        message: 'orders deleted',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router;
